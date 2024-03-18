ddd53c5d993e5e0cd6f4291b2320f58e8412035a
kubwosz <kubwosz@gmail.com>
2024-03-18 21:49:47 +0100

    fix environment

--- citatio-frontend/src/components/Home.jsx
+++ citatio-frontend/src/components/Home.jsx
@@ -53,28 +53,80 @@ const Home = (props) => {
   }
 
   return (
-    <section>
-      <div className="container w-75 pb-4">
-        <form onSubmit={submitHandler}>
-          <div className="form-group pb-3">
-            <label htmlFor="title">Put here your DOI</label>
-            <input id="title" type="text" className="form-control" required value={doiValue} onChange={doiInputChangeHandler}></input>
-          </div>
-          <button type="button" onClick={submitHandler} className="btn btn-success">Get DOI information</button>
-        </form>
-        <h2>Information about your DOI:</h2>
-        <ul class="list-group">
-        <input
-                 type="button"
-                 className={butonActivity === true ? 'active' : ''}
-                 value="value1"
-                 onClick={() => setButton(!butonActivity)}
-                 />;
-  <li class="list-group-item"><span class="label label-default">Your reference AAA:</span>   {doiData} </li>
-  <li class="list-group-item"><span class="label label-default">Your reference APA:</span>   {doiDataFormat1} </li>
-</ul>
-      </div>
-    </section>
+    <div className="">
+    <div className="leftPanel flex-auto">
+      <label htmlFor="title">Put here your DOI</label>
+      <input
+        id="title"
+        type="text"
+        className="form-control"
+        required
+        value={doiValue}
+        onChange={doiInputChangeHandler}
+      ></input>
+      <button
+        type="button"
+        onClick={submitHandler}
+        className="btn btn-success"
+      >
+        Get DOI information
+      </button>
+    </div>
+    <div className="rightPanel flex-auto">
+      <h2>Information about your DOI:</h2>
+      <ul class="list-group">
+        <div class="flex">
+          <div class="flex-none w-14 h-14">01</div>
+          <div class="flex-initial w-64">02</div>
+          <div class="flex-initial w-32 ">03</div>
+        </div>
+        <button
+          type="button"
+          class="btn btn-primary btn-aaa"
+          data-toggle="button"
+          aria-pressed="false"
+          autocomplete="off"
+        >
+          Single toggle
+        </button>
+        <button
+          type="button"
+          class="btn btn-primary btn-apa"
+          data-toggle="button"
+          aria-pressed="false"
+          autocomplete="off"
+        >
+          Single toggle
+        </button>
+        <button
+          type="button"
+          class="btn btn-primary btn-aaa"
+          data-toggle="button"
+          aria-pressed="false"
+          autocomplete="off"
+        >
+          Single toggle
+        </button>
+        <button
+          type="button"
+          class="btn btn-primary"
+          data-toggle="button"
+          aria-pressed="false"
+          autocomplete="off"
+        >
+          Single toggle
+        </button>
+        <li class="list-group-item">
+          <span class="label label-default">Your reference AAA:</span>{" "}
+          {doiData}{" "}
+        </li>
+        <li class="list-group-item">
+          <span class="label label-default">Your reference APA:</span>{" "}
+          {doiDataFormat1}{" "}
+        </li>
+      </ul>
+    </div>
+  </div>
   );
 }
 

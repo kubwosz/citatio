da85c5b7de1eed2df7b1392ce0883c65cff5f104
kubwosz <kubwosz@gmail.com>
2024-03-25 23:04:49 +0100

    Add more cite st=yles - not fully implemented

---
+++ internal/domain/cite_styles/harvard.go
@@ -0,0 +1,14 @@
+package cite_styles
+
+import (
+	"citatio/internal/domain/manipulators"
+	"citatio/internal/domain/models"
+	"fmt"
+)
+
+func (p ReferenceSource) Harvard(paper models.Paper) (reference string) {
+	names := manipulators.GetSurnameAndFirstLetterOfName(paper)
+
+	reference = fmt.Sprintf("%s %v. \"%s\" %s. %v", names, paper.Year, paper.Title, paper.Publisher, paper.Link)
+	return
+}

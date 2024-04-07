a9a150c1a19d07b77b8709b905deb4ba85779010
kubwosz <kubwosz@gmail.com>
2024-04-07 23:34:04 +0200

    Add tags concept and displaying

--- citatio-frontend/src/components/InputTags.jsx
+++ citatio-frontend/src/components/InputTags.jsx
@@ -2,33 +2,47 @@ import { useState } from "react";
 
 const InputTags = (props) => {
     const [tags, setTags] = useState([]);
+    const [inputValue, setInputValue] = useState(props.doiValue);
 
-    const addIfIsATag = (stringValue) => {
-        let re = new RegExp('(10[.][0-9]{4,}[^\s"/<>]*/[^\s"<>]+)');
 
-        console.log(re.test(stringValue));
-        if(re.test(stringValue)){
-            tags.push(stringValue);
-        }
+    const addTag = (tagValue) => {
+            tags.push(tagValue);
         console.log(tags);
-      };
+    };
+
+    const isADoi = (stringValue) => {
+        let re = new RegExp('(10[.][0-9]{4,}[^\s"/<>]*/[^\s"<>]+)');
+        return re.test(stringValue)
+    };
 
     const inputChangeHandler = (data) => {
-        addIfIsATag(data.target.value)
-        props.doiInputChangeHandler(data);
-      };
+        if(isADoi(data.target.value)){
+            console.log("isadoi");
+            addTag(data.target.value)
+            setInputValue('');
+        }
+        setInputValue(data.target.value);
+    };
 
     return (
-            <input
-          id="title"
-          className="block bg-rose-200 w-3/12 hover:bg-green-500 text-white py-2 rounded m-3 self-center"
-          placeholder=" DOI..."
-          type="text"
-          required
-          value={props.doiValue}
-          onChange={inputChangeHandler}
-        ></input>
-        )
+        <div className="relative"> 
+        <div className="absolute z-2">
+        {tags.map((tag, idx) => (
+             <span className=" border-2 rounded-sm border-red-950">{tag}</span>
+            ))}
+            </div>
+        <input
+            id="title"
+            className="block bg-rose-200 w-3/12 hover:bg-green-500 text-white py-2 rounded m-3 self-center"
+            placeholder=" DOI..."
+            type="text"
+            required
+            value={inputValue}
+            onChange={inputChangeHandler}
+        >
+        </input>
+        </div>
+    )
 };
 
 export default InputTags;

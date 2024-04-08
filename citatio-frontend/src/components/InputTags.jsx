6b91d6465697d8387d31a75cf1690553649c6fe8
kubwosz <kubwosz@gmail.com>
2024-04-08 22:15:44 +0200

    Add working regex reading and saving to tags

--- citatio-frontend/src/components/InputTags.jsx
+++ citatio-frontend/src/components/InputTags.jsx
@@ -5,42 +5,52 @@ const InputTags = (props) => {
     const [inputValue, setInputValue] = useState(props.doiValue);
 
 
-    const addTag = (tagValue) => {
-            tags.push(tagValue);
+    const addTags = (tagValues) => {
+        tagValues.map(tag => {
+            if(tags.indexOf(tag) === -1) {
+                tags.push(tag);
+            }
+        })
         console.log(tags);
+        console.log(tagValues);
     };
 
-    const isADoi = (stringValue) => {
+    const getDois = (stringValue) => {
         let re = new RegExp('(10[.][0-9]{4,}[^\s"/<>]*/[^\s"<>]+)');
-        return re.test(stringValue)
+        return re.exec(stringValue);
     };
 
     const inputChangeHandler = (data) => {
-        if(isADoi(data.target.value)){
-            console.log("isadoi");
-            addTag(data.target.value)
-            setInputValue('');
+        let dois = getDois(data.target.value);
+        console.log(dois);
+        if (dois?.length > 0) {
+            addTags(dois)
+            dois.map(doi => {
+                let inputToUpdate = inputValue.replace(doi, '');
+                setInputValue(inputToUpdate);
+            })
+        } else {
+            setInputValue(data.target.value);
         }
-        setInputValue(data.target.value);
     };
 
     return (
-        <div className="relative"> 
-        <div className="absolute z-2">
-        {tags.map((tag, idx) => (
-             <span className=" border-2 rounded-sm border-red-950">{tag}</span>
-            ))}
+        <div className="relative">
+            <div className="absolute z-2">
+                {tags.map((tag, idx) => (
+                    <span className=" border-2 rounded-sm border-red-950">{tag}</span>
+                ))}
             </div>
-        <input
-            id="title"
-            className="block bg-rose-200 w-3/12 hover:bg-green-500 text-white py-2 rounded m-3 self-center"
-            placeholder=" DOI..."
-            type="text"
-            required
-            value={inputValue}
-            onChange={inputChangeHandler}
-        >
-        </input>
+            <input
+                id="title"
+                className="block bg-rose-200 w-3/12 hover:bg-green-500 text-white py-2 rounded m-3 self-center"
+                placeholder=" DOI..."
+                type="text"
+                required
+                value={inputValue}
+                onChange={inputChangeHandler}
+            >
+            </input>
         </div>
     )
 };

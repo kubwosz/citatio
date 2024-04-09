58c94c3cd55b787b2a9e5e12e5856d65d6aaff2b
kubwosz <kubwosz@gmail.com>
2024-04-09 22:54:47 +0200

    Add bulk dois toggle button, add two types of dois

--- citatio-frontend/src/components/InputTags.jsx
+++ citatio-frontend/src/components/InputTags.jsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 const InputTags = (props) => {
     const [tags, setTags] = useState([]);
     const [inputValue, setInputValue] = useState(props.doiValue);
+    const [bulkDoi, setBulkDoi] = useState(false);
+    const [isInputValueDoi, setIsInputValueDoi] = useState(false);
 
 
     const addTags = (tagValues) => {
@@ -20,6 +22,11 @@ const InputTags = (props) => {
         return re.exec(stringValue);
     };
 
+    const isDoi = (stringValue) => {
+        let re = new RegExp('(10[.][0-9]{4,}[^\s"/<>]*/[^\s"<>]+)');
+        return re.test(stringValue);
+    };
+
     const inputChangeHandler = (data) => {
         let dois = getDois(data.target.value);
         console.log(dois);
@@ -34,6 +41,14 @@ const InputTags = (props) => {
         }
     };
 
+    const inputNoDoiChangeHandler = (data) => {
+       console.log(bulkDoi);
+            setInputValue(data.target.value);
+            if(isDoi(data.target.value)){
+                setIsInputValueDoi(true);
+            }
+    };
+
     return (
         <div className="relative">
             <div className="absolute z-2">
@@ -41,16 +56,32 @@ const InputTags = (props) => {
                     <span className=" border-2 rounded-sm border-red-950">{tag}</span>
                 ))}
             </div>
-            <input
+            {bulkDoi ? 
+            <textarea
                 id="title"
-                className="block bg-rose-200 w-3/12 hover:bg-green-500 text-white py-2 rounded m-3 self-center"
+                className={`resize-y max-h-32 min-h-12 block bg-amber-100 w-3/12 hover:bg-amber-200 py-2 rounded m-3 self-center inline-block ${isInputValueDoi ? 'border-2 border-green-500' : ''}`}
                 placeholder=" DOI..."
                 type="text"
                 required
                 value={inputValue}
-                onChange={inputChangeHandler}
-            >
-            </input>
+                onChange={inputNoDoiChangeHandler}
+            />
+            :
+            <input
+                id="title"
+                className={`block bg-amber-100 w-3/12 hover:bg-amber-200 py-2 rounded m-3 self-center inline-block ${isInputValueDoi ? 'border-2 border-green-500' : ''}`}
+                placeholder=" DOI..."
+                type="text"
+                required
+                value={inputValue}
+                onChange={inputNoDoiChangeHandler}
+            />
+            }
+            <label class="inline-flex items-center cursor-pointer inline-block">
+  <input type="checkbox" value={bulkDoi} onChange={() => setBulkDoi(!bulkDoi)} className="sr-only peer"/>
+  <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+  <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Bulk dois</span>
+</label>
         </div>
     )
 };

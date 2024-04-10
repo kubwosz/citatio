398cb10a9cf38cf385bd27e035eb42608cbc0326
kubwosz <kubwosz@gmail.com>
2024-04-10 23:10:55 +0200

    Proper elements positioning

--- citatio-frontend/src/components/InputTags.jsx
+++ citatio-frontend/src/components/InputTags.jsx
@@ -50,17 +50,17 @@ const InputTags = (props) => {
     };
 
     return (
-        <div className="relative">
-            <div className="absolute z-2">
+        <div className="flex flex-row justify-center relative">
+            {/* <div className="absolute z-2">
                 {tags.map((tag, idx) => (
                     <span className=" border-2 rounded-sm border-red-950">{tag}</span>
                 ))}
-            </div>
+            </div> */}
             {bulkDoi ? 
             <textarea
                 id="title"
-                className={`resize-y max-h-32 min-h-12 block bg-amber-100 w-3/12 hover:bg-amber-200 py-2 rounded m-3 self-center inline-block ${isInputValueDoi ? 'border-2 border-green-500' : ''}`}
-                placeholder=" DOI..."
+                className={`resize-y max-h-32 min-h-12 block self-center bg-amber-100 w-3/12 hover:bg-amber-200 py-2 rounded m-3 self-center inline-block ${isInputValueDoi ? 'border-2 border-green-500' : ''}`}
+                placeholder=" DOIs..."
                 type="text"
                 required
                 value={inputValue}
@@ -77,7 +77,7 @@ const InputTags = (props) => {
                 onChange={inputNoDoiChangeHandler}
             />
             }
-            <label class="inline-flex items-center cursor-pointer inline-block">
+            <label class="sticky left-0 cursor-pointer inline-block">
   <input type="checkbox" value={bulkDoi} onChange={() => setBulkDoi(!bulkDoi)} className="sr-only peer"/>
   <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
   <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Bulk dois</span>
